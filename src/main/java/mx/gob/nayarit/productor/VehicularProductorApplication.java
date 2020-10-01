4216e66032afe12aef4e2b673fba1fdc1950f1ac
ricdev2 <ricardo.professional.dev@gmail.com>
2020-10-01 12:54:25 -0500

    Initial commit

---
+++ src/main/java/mx/gob/nayarit/productor/VehicularProductorApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.nayarit.productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicularProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicularProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/nayarit/productor/VehicularProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.nayarit.productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicularProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
